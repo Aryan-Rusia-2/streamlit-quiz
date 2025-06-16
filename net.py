a91b920136bbf3959f74eca51dd74c9d46bd0a79
Aryan Rusia <110370121+Aryan-Rusia-2@users.noreply.github.com>
2025-06-16 23:22:04 +0530

    Add files via upload

--- net.py
+++ net.py
@@ -20,13 +20,15 @@ if paper_choice == "Paper 1":
 
 else:
     st.subheader("Paper 2 – English Literature")
-    quiz_type = st.selectbox("Quiz Type", ["Indian Literature", "Cultural Studies", "Chronology", "Literary Theory"])
+    quiz_type = st.selectbox("Quiz Type", ["Indian Literature", "Cultural Studies", "Chronology", "Literary Theory", "Linguistics"])
     if quiz_type == "Indian Literature":
         json_file = "chapterOne.json"
     elif quiz_type == "Cultural Studies":
         json_file = "chapterTwo.json"
     elif quiz_type == "Literary Theory":
         json_file = "chapterThree.json"
+    elif quiz_type == "Linguistics":
+        json_file = "linguistics.json"
     else:
         json_file = "chronology.json"
 
